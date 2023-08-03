9af7a0673a47737abbbe144ef66d8652c9f41317
Hung16112007 <125101945+Hung16112007@users.noreply.github.com>
2023-08-03 19:45:07 +0700

    config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'yêu lắm <33',
-    btnNo: 'ko thấy 500k thôi :3',
+    btnNo: 'ko lấy 500k thôi :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
